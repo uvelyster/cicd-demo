40e1c827eb57d63be72ea7d1998c781dbb5d59e0
uvelyster <48861250+uvelyster@users.noreply.github.com>
2024-05-23 16:33:00 +0900

    Update run-artifactory.sh

--- artifactory/run-artifactory.sh
+++ artifactory/run-artifactory.sh
@@ -5,4 +5,4 @@ docker rm -f artifactory
 docker run -d --name artifactory \
  -p 8082:8082 -p 8081:8081 \
  -v artifactory-data:/var/opt/jfrog/artifactory \
- docker.bintray.io/jfrog/artifactory-oss:latest
+ releases-docker.jfrog.io/jfrog/artifactory-oss:7.77.5
